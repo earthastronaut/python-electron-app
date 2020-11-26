9551508dfad046cf6363aa540547bb7afe47dc90
dylan <dylan@verdigris.co>
2020-11-26 03:31:55 -0700

    have flask run as child process not separate process

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-const {app, BrowserWindow} = require('electron')
-const pyshell = require('python-shell');
+const {app, BrowserWindow} = require('electron');
+const {spawn} = require('child_process');
+
+// todo: must be a better way! without using a global variable.
+var mainpy;
+
+function initializePythonProcess() {
+  const mainpy = spawn('python3', ['main.py']);
+
+  mainpy.stdout.on('data', (data) => {
+    console.log(`mainpy: info: ${data}`);
+  })
+
+  mainpy.stderr.on('data', (data) => {
+    console.log(`mainpy: error: ${data}`)
+  })
+
+  mainpy.on('close', (code) => {
+    console.log(`mainpy exited with code ${code}`);
+  });
+
+  return mainpy
+}
 
 function createWindow () {
-    window = new BrowserWindow({width: 800, height: 600})
-    window.loadFile('index.html')
+  window = new BrowserWindow({width: 800, height: 600});
+  window.loadFile('index.html');
 
-pyshell.run(
-  'main.py',
-  (err, results) => {
-    if (err) throw err;
-  });
+  mainpy = initializePythonProcess();
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
+  app.quit()
+})
+
+app.on('quit', () => {
+  if (mainpy) {mainpy.kill(2)};
 })
