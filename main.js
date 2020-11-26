2205be2c070750bcbfd035a16f66644fe244fa94
dylan <dylan@verdigris.co>
2020-11-26 02:22:27 -0700

    modify electron app to use the flask python main

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
 const {app, BrowserWindow} = require('electron')
+const pyshell = require('python-shell');
 
 function createWindow () {
     window = new BrowserWindow({width: 800, height: 600})
     window.loadFile('index.html')
 
-
-    	/*var python = require('child_process').spawn('python', ['./hello.py']);
-	python.stdout.on('data',function(data){
-    		console.log("data: ",data.toString('utf8'));
-	});*/
-
-
-var pyshell =  require('python-shell');
-
-pyshell.run('hello.py',  function  (err, results)  {
- if  (err)  throw err;
- console.log('hello.py finished.');
- console.log('results', results);
-});   	
-    
+pyshell.run(
+  'main.py',
+  (err, results) => {
+    if (err) throw err;
+  });
 }
-
-
 
 app.on('ready', createWindow)
 
@@ -30,4 +19,3 @@ app.on('window-all-closed', () => {
       app.quit()
     }
 })
-
